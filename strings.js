9bab9a33eb948ad081ee7ecc0067dcccaf8c818d
Jayant <39442192+RoguedBear@users.noreply.github.com>
2023-07-05 11:54:48 +0530

    github treats newline literally as newline apprently in markdown

--- strings.js
+++ strings.js
@@ -1,20 +1,18 @@
 export function getCommentMessage(oldBranchName) {
     // TODO: add a list of allowed branches in herewhen that part is implemnted
     return `
-# ⚠️⚠️ Your branch name \`${oldBranchName}\` does not conform with the branch naming strategies defined in your repository
+## ⚠️⚠️ Your branch name \`${oldBranchName}\` does not conform with the branch naming strategies defined in your repository
 
 These are the branch naming schemes defined in this repository:
 ${"```"}
 <BRANCH REGEXES>
 ${"```"}
 
-but do not worry, you just have to rename the branch you have created to pass
-this check
+but do not worry, you just have to rename the branch you have created to pass this check
 
 ## Renaming your branch locally and on remote
 
-pick any of the prefix that applies to your changes, and replace \`<new_name>\`
-with that.
+pick any of the prefix that applies to your changes, and replace \`<new_name>\` with that.
 
 example: \`feature/${oldBranchName}\`
 
